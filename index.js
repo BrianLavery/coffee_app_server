09c245f3cc494b84851eb13eda0d5d94468e9024
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-09-28 11:26:01 +0700

    added nodemon to project

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const customerRoutes = require('./routes/customerRoutes');
 
 const app = express();
-
 customerRoutes(app);
 
 const PORT = process.env.PORT || 5000;
