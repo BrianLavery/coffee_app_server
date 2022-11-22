3a9cc98e295d5a0a5c2a64849f39b537fcb9d4ec
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-11-22 18:02:42 +0700

    added some non library dependent code to force ssl ;

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-import sslRedirect from 'heroku-ssl-redirect'; // https://medium.com/@seunghunsunmoonlee/how-to-enforce-https-redirect-http-to-https-on-heroku-deployed-apps-a87a653ba61e
+// import sslRedirect from 'heroku-ssl-redirect'; // https://medium.com/@seunghunsunmoonlee/how-to-enforce-https-redirect-http-to-https-on-heroku-deployed-apps-a87a653ba61e
 
 const keys = require('./config/keys');
 require('./models/User'); // must be executed before routes
 require('./models/Click'); // must be executed before routes
 const customerRoutes = require('./routes/customerRoutes');
 const clickRoutes = require('./routes/clickRoutes');
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+const environment = process.env.NODE_ENV;
+
 // Force https / SSL
-app.use(sslRedirect()); // https://medium.com/@seunghunsunmoonlee/how-to-enforce-https-redirect-http-to-https-on-heroku-deployed-apps-a87a653ba61e
+// app.use(sslRedirect()); // https://medium.com/@seunghunsunmoonlee/how-to-enforce-https-redirect-http-to-https-on-heroku-deployed-apps-a87a653ba61e
 
 // create application/json parser
 app.use(bodyParser.json());
@@ -24,8 +26,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 customerRoutes(app);
 clickRoutes(app);
 
+const forceSsl = (req, res, next) => {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		return res.redirect(['https://', req.get('Host'), req.url].join(''));
+	}
+	return next();
+};
+
+app.configure(() => {
+	if (environment === 'production') {
+		app.use(forceSsl);
+	}
+});
+
 // Code below only runs on production
-if (process.env.NODE_ENV === 'production') {
+if (environment === 'production') {
 	app.use(express.static('client/build')); // If a request is coming then look for specific answer in the 'client/build' directory
 
 	const path = require('path');
